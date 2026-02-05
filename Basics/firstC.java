9185859e53c3c2e294b3d50f5a5ee484eee33114
[sneha suman] <snehasuman1119@gmail.com>
2026-02-05 22:53:43 +0530

    Patterns

--- Basics/firstC.java
+++ Basics/firstC.java
@@ -2,5 +2,11 @@ class firstC{
     public static void main(String[] args) {
         
         System.out.println("Chlo bhai suru ho jao!!!");
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+
     }
 }
