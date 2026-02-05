12746c29b0b1f855341d182694a9a946a1b66aee
[sneha suman] <snehasuman1119@gmail.com>
2026-02-05 23:12:08 +0530

    Variables

---
+++ Basics/variables.java
@@ -0,0 +1,13 @@
+// package Basics;
+
+public class variables {
+    public static void main(String[] args) {
+        String name = "HEllo jiii";
+        int age = 48;
+        double price = 35.45;
+        int a = 30;
+        int b = 25;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
