6812ffc425ae449f7d9cd0930c75491df7b5f7c7
[sneha suman] <snehasuman1119@gmail.com>
2026-02-06 00:23:03 +0530

    Odd even

--- Basics/conditions.java
+++ Basics/conditions.java
@@ -3,12 +3,18 @@ import java.util.*;
 public class conditions {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int age = sc.nextInt();
-        if(age>18){
+        int x = sc.nextInt();
+        /*if(age>18){
             System.out.println("Adult");
         }else{
             System.out.println("Not adult");
         }
+*/
+    if(x%2==0){
+        System.out.println("Even");
+    }else{
+        System.out.println("odd");
+    }
     }
     
 }
