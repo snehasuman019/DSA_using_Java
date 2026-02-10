8d9ad9ccf5669d540932fe4c72fe618eafa9880c
[sneha suman] <snehasuman1119@gmail.com>
2026-02-10 21:25:42 +0530

    Sum of first n natural numbers

---
+++ Basics/sumofNno.java
@@ -0,0 +1,13 @@
+// Print the sum of first n natural numbers 
+
+import java.util.*;
+public class sumofNno {
+    public static void main(String[] args){
+        int n=4;
+        int sum =0;
+        for(int i=1;i<=n;i++){
+            sum = sum +i;
+        }
+        System.out.println(sum);
+    }
+}
