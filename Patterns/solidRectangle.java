ef425d598d18a62939fbd0045c24c9da2cb50c92
[sneha suman] <snehasuman1119@gmail.com>
2026-02-10 22:00:20 +0530

    PAtterns

---
+++ Patterns/solidRectangle.java
@@ -0,0 +1,15 @@
+// package Patterns;
+import java.util.*;
+public class solidRectangle {
+    public static void main(String[] args){
+        int n = 4;
+        int m = 4;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
