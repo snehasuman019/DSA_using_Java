58eac5f1ee3d370762d856cc0b604d137204c85a
[sneha suman] <snehasuman1119@gmail.com>
2026-02-10 22:01:43 +0530

    Solid rectangle

--- Patterns/solidRectangle.java
+++ Patterns/solidRectangle.java
@@ -2,8 +2,8 @@
 import java.util.*;
 public class solidRectangle {
     public static void main(String[] args){
-        int n = 5;
-        int m = 4;
+        int n = 6;
+        int m = 5;
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                 System.out.print("*");
