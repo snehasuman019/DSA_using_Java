f667ff71bef3ca05cf9d90eb402d231b6eda9ffc
[sneha suman] <snehasuman1119@gmail.com>
2026-02-10 23:54:29 +0530

    Do while

---
+++ Basics/dowhileloo.java
@@ -0,0 +1,10 @@
+public class dowhileloo {
+    public static void main(String[] args) {
+        int i = 0;
+        do{
+            System.err.println(i);
+            i = i+1;
+
+        }while(i<11);
+    }    
+}
