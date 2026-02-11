c11ba402ed9ce77eea68b72575d02390ea5775e2
[sneha suman] <snehasuman1119@gmail.com>
2026-02-12 00:01:47 +0530

    Hollow Rhombus

---
+++ Patterns/HollowRhombus.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class HollowRhombus {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i=1; i<=n; i++){
+            // spaces before rhombus
+            for(int s = 1; s<= n-i; s++)
+                System.out.print(" ");
+            // hollow square
+            for(int j=1; j<=n;j++){
+                if(i==1 || i==n || j== 1 || j==n)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
