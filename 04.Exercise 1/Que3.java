78fc3aaa07c59d68e746ecf73d456c84b28f76ba
[sneha suman] <snehasuman1119@gmail.com>
2026-02-15 22:44:13 +0530

    Greater among two numbers

---
+++ 04.Exercise 1/Que3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Que3 {
+    public static int findGreaterNum(int a, int b){
+        if(a >b){
+            return a;
+        }else{
+            return b;
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(findGreaterNum(a,b));
+    }
+}
