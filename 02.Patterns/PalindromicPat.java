87b7ebfbcfec900964c7ba5a50057dba56cc9308
[sneha suman] <snehasuman1119@gmail.com>
2026-02-15 16:01:35 +0530

    Pascal's Triangle

---
+++ 02.Patterns/PalindromicPat.java
@@ -0,0 +1,20 @@
+public class PalindromicPat {
+    public static void main(String[] args){
+        int n=5;
+        for(int i=1; i<=n; i++){
+            // spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            // !st half num
+            for(int j=i; j>=1;j--){
+                System.out.print(j);
+            }
+            // 2nd half
+            for(int j=2;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ 02.Patterns/solidRectangle.java
@@ -0,0 +1,15 @@
+// package Patterns;
+import java.util.*;
+public class solidRectangle {
+    public static void main(String[] args){
+        int n = 6;
+        int m = 5;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
