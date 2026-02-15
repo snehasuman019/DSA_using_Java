6de977db1e530bedd1132db94249ce8f94308a0b
[sneha suman] <snehasuman1119@gmail.com>
2026-02-15 23:29:23 +0530

    Value of a number raised to power of another

---
+++ 04.Exercise 1/Que8.java
@@ -0,0 +1,18 @@
+// Two numbers are entered by the user, x and n. Write a function to find the value of one number raised to the power of another i.e. xn.
+
+import java.util.*;
+public class Que8 {
+    public static void main(String[] args) {
+        System.out.println("Enter x");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println("Enter n");
+        int n = sc.nextInt();
+        int result = 1;
+        for(int i=0; i<n; i++){
+            result = result * x;
+
+        }
+        System.out.println("x to the power n is: "+ result);
+    }
+}
