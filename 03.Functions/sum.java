a9d32526ed0dafcd39bd12c4d65b2bacf1675de4
[sneha suman] <snehasuman1119@gmail.com>
2026-02-15 17:09:34 +0530

    Sum of 2 numbers

---
+++ 03.Functions/sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class sum {
+    public static void printSum(int sum){
+        System.out.println(sum);
+        return;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        printSum(sum);
+    }
+}
