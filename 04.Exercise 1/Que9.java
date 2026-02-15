c6453ee477f9e313a827152db2cd363acf1d8dc6
[sneha suman] <snehasuman1119@gmail.com>
2026-02-15 23:38:18 +0530

    GCD of two numbers

---
+++ 04.Exercise 1/Que9.java
@@ -0,0 +1,18 @@
+// Write a function that calculates the Greatest Common Divisor of 2 numbers. 
+
+import java.util.*;
+public class Que9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2 = sc.nextInt();
+        while(n1 != n2){
+            if(n1 > n2){
+                n1 = n1-n2;
+            }else{
+                n2 = n2-n1;
+            }
+        }
+        System.out.println("GCD is: "+n2);
+    }
+}
