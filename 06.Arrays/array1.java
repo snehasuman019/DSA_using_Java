491b47e87df2a8eccdfb4da88aa206f66d0871dc
[sneha suman] <snehasuman1119@gmail.com>
2026-02-17 13:40:48 +0530

    Arrays using loops

--- 06.Arrays/array1.java
+++ 06.Arrays/array1.java
@@ -2,12 +2,17 @@
 import java.util.*;
 public class array1 {
     public static void main(String[] args){
-        int[] marks = new int[20];
+        int[] marks = new int[3];
         marks[0] = 91;
         marks[1] = 92;
         marks[2] = 80;
-        System.out.println(marks[1]);
-        System.out.println(marks[0]);
-        System.out.println(marks[2]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[0]);
+        // System.out.println(marks[2]);
+    // using loops
+
+        for(int i=0; i<3;i++){
+            System.out.println(marks[i]);
+        }
     }
 }
