d2c888f428a0a813d4de7792bbb440a9b71a8b34
[sneha suman] <snehasuman1119@gmail.com>
2026-02-17 13:26:51 +0530

    Arrays

---
+++ 06.Arrays/array1.java
@@ -0,0 +1,11 @@
+// 
+import java.util.*;
+public class array1 {
+    public static void main(String[] args){
+        int[] marks = new int[20];
+        marks[0] = 91;
+        marks[1] = 92;
+        marks[2] = 80;
+        System.out.println(marks[1]);
+    }
+}
