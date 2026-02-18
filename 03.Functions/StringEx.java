0f7e0261b76ef4ef832c24f629e45a6842e11525
[sneha suman] <snehasuman1119@gmail.com>
2026-02-18 22:42:10 +0530

    Greet

---
+++ 03.Functions/StringEx.java
@@ -0,0 +1,12 @@
+public class StringEx {
+    static String greet(){
+        String greeting = "How are you";
+        return greeting;
+    }
+    public static void main(String[] args) {
+        String msg = greet();
+        System.out.println(msg);
+        
+    }
+    
+}
