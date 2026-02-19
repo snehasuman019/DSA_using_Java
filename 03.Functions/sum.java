3a588683a9498ee0d931aa52e87ae95320302c9f
[sneha suman] <snehasuman1119@gmail.com>
2026-02-19 10:16:35 +0530

    Sum

--- 03.Functions/sum.java
+++ 03.Functions/sum.java
@@ -1,19 +1,68 @@
-// sum and multiplication of a number
+// // sum and multiplication of a number
 
-import java.util.*;
+// import java.util.*;
+// public class sum {
+//     public static void calculateNum(int sum){
+//         System.out.println(sum);
+//         return;
+
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int sum = a + b;
+//         int d = a * b;
+//         calculateNum(sum);
+//         System.out.println(d);
+//     }
+// }
+
+
+import java.util.Scanner;
+
 public class sum {
-    public static void calculateNum(int sum){
-        System.out.println(sum);
-        return;
-
-    }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int sum = a + b;
-        int d = a * b;
-        calculateNum(sum);
-        System.out.println(d);
+//        int ans = sum2();
+//        System.out.println(ans);
+
+        int ans = sum3(20, 30);
+        System.out.println(ans);
     }
+
+    // pass the value of numbers when you are calling the method in main()
+    static int sum3(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    // return the value
+    static int sum2() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter number 1: ");
+        int num1 = in.nextInt();
+        System.out.print("Enter number 2: ");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        return sum;
+//        System.out.println("This will never execute");
+    }
+
+    static void sum() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter number 1: ");
+        int num1 = in.nextInt();
+        System.out.print("Enter number 2: ");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+    }
+
+    /*
+        return_type name (arguments) {
+            // body
+            return statement;
+        }
+
+     */
 }
