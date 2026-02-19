b2dc95d66a6c9fcba8773327e2ac9337a2e70062
[sneha suman] <snehasuman1119@gmail.com>
2026-02-19 10:19:40 +0530

    Swap.java

--- 03.Functions/swap.java
+++ 03.Functions/swap.java
@@ -1,13 +1,32 @@
+
+
 public class swap {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        // swap numbers code
 
-        int temp = a;
-        a=b;
-        b=temp;
-        System.out.println("a: "+ a+ " " + "b: " + b);
+        // swap numbers code
+//        int temp = a;
+//        a = b;
+//        b = temp;
+
+        swap(a, b);
+
+        System.out.println(a + " " + b);
+
+        String name = "Sneha Suman";
+        changeName(name);
+        System.out.println(name);
     }
-    
+
+    static void changeName(String naam) {
+        naam = "Senu"; // creating a new object
+    }
+
+    static void swap(int num1, int num2) {
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+        // this change will only be valid in this function scope only.
+    }
 }
