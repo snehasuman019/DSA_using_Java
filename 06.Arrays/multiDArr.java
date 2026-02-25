21468144fae2e35901e48a7fe555b3085f72a464
[sneha suman] <snehasuman1119@gmail.com>
2026-02-25 10:03:28 +0530

    Multi dimensional array

--- 06.Arrays/multiDArr.java
+++ 06.Arrays/multiDArr.java
@@ -11,6 +11,8 @@ public class multiDArr {
         //     {6,7,8,9}   //2nd index  -> arr2D[2] = {6,7,8,9}
         // };
         Scanner in = new Scanner(System.in);
+
+        /*/
         int[][] arr = new int[3][3];
         // input
         System.out.println(arr.length);     //no of rows
@@ -30,6 +32,23 @@ public class multiDArr {
             }
             System.out.println();
         }
+        */
+
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        // initialization
+        for(int i=0; i<3; i++){
+            list.add(new ArrayList<>());
+
+        }
+        // add elements
+        for(int i=0; i<3; i++){
+            for(int j =0; j<3; j++){
+                list.get(i).add(in.nextInt());
+            }
+        }
+        System.out.println(list);
+
+
     }
     
 }
