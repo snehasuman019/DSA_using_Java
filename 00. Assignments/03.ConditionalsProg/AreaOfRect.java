2f1ec091f913a1592bc927cfb27c3da9d910906c
[sneha suman] <snehasuman1119@gmail.com>
2026-02-27 00:15:55 +0530

    Area of Rectangle

---
+++ 00. Assignments/03.ConditionalsProg/AreaOfRect.java
@@ -0,0 +1,16 @@
+// Area Of Rectangle Program
+
+
+import java.util.*;
+public class AreaOfRect {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of the rectangle: ");
+        double length = sc.nextDouble();
+        System.out.print("Enter the width of the rectangle: ");
+        double width = sc.nextDouble();
+        double area = length * width;
+        System.out.println("The area of rectangle is : "+ area);
+    }
+    
+}
