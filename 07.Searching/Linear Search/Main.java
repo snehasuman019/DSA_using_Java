13241b23408448d0fd529c713f1614844de043aa
[sneha suman] <snehasuman1119@gmail.com>
2026-03-05 23:14:51 +0530

    Searching

---
+++ 07.Searching/Linear Search/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {23, 45, 1, 2, 8, 19, -3, 16,-11, 28};
+        int target = 19;
+        int ans = linearSearch(nums, target);
+        System.out.println(ans);
+    }
+
+    // search in the array : return the index if found , otherwise return -1
+    static int linearSearch(int[] arr, int target){
+        if(arr.length ==0){
+            return -1;
+        }
+        // for loop
+        for(int i =0; i<arr.length; i++){
+            // check for element at every index if it is = target 
+            int element = arr[i];
+            if(element == target){
+                return i;
+            }
+        }
+        // this line will excecute if none of the return statement above have executed 
+        // hence the target not found
+        return -1;
+    }
+}
