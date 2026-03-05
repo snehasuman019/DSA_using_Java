5916bb2a4430c009d5659288788693b1d988a463
[sneha suman] <snehasuman1119@gmail.com>
2026-03-06 00:47:14 +0530

    Linear Search

--- 07.Searching/Linear Search/Main.java
+++ 07.Searching/Linear Search/Main.java
@@ -2,11 +2,13 @@ public class Main {
     public static void main(String[] args) {
         int[] nums = {23, 45, 1, 2, 8, 19, -3, 16,-11, 28};
         int target = 19;
-        int ans = linearSearch(nums, target);
+        // int ans = linearSearch2(nums, target);
+        boolean ans = linearSearch3(nums, target);
         System.out.println(ans);
     }
 
     // search in the array : return the index if found , otherwise return -1
+/*/
     static int linearSearch(int[] arr, int target){
         if(arr.length ==0){
             return -1;
@@ -22,5 +24,40 @@ public class Main {
         // this line will excecute if none of the return statement above have executed 
         // hence the target not found
         return -1;
+
+    */
+/*/
+// search for the target : return the element if found , otherwise return -1
+    static int linearSearch2(int[] arr, int target){
+        if(arr.length == 0){
+            return -1;
+        }
+        for(int element : arr){
+            if(element == target){
+                return element;
+            }
+        }
+        return -1;
+*/
+
+// search the target : return true if found , otherwise return false
+         static boolean linearSearch3(int[] arr, int target) {
+        if (arr.length == 0) {
+            return false;
+        }
+
+        // run a for loop
+        for (int element : arr) {
+            if (element == target) {
+                return true;
+            }
+        }
+        // this line will execute if none of the return statements above have executed
+        // hence the target not found
+        return false;
     }
 }
+// time complexity : 
+// Best Case O(1) // constant   
+// Worst Case O(N)  //N ->size of array
+// Average Case O(N) //N ->size of array
