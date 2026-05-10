8114599242f2bdef188269bb7287d4efe9bb6733
[sneha suman] <snehasuman1119@gmail.com>
2026-05-10 14:29:18 +0530

    Reverse of a number

--- Rev Prac/Facto.java
+++ Rev Prac/Facto.java
@@ -5,8 +5,8 @@ public class Facto {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int n = sc.nextInt();
-        int fact =1;
-        for(int i =1;i<=n; i++){
+        int fact = 1;
+        for(int i = 1;i<=n; i++){
             fact *= i;
         }    
         System.out.println("Factorial of " + n + " is: " + fact);
