aa6e6d540c71f124c23a5a31951049aed39e7231
[sneha suman] <snehasuman1119@gmail.com>
2026-05-10 12:37:48 +0530

    Largest No

---
+++ Rev Prac/LArgestNo.java
@@ -0,0 +1,25 @@
+// package Rev Prac;
+
+import java.util.*;
+import java.util.Scanner;
+public class LArgestNo {
+    public static void main(String[] args){
+        // int a = 10;
+        // int b = 20;
+        // int c = 30;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 3 numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if(a > b && a > c){
+            System.out.println("Largest number is: " + a);
+        }
+        else if(b > a && b > c){
+            System.out.println("Largest number is: " + b);
+        }
+        else{
+            System.out.println("Largest number is: " + c);
+        }
+    }
+}
