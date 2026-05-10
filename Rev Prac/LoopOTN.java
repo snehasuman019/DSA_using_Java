da4ff0410a80220ec409c77edf1fc082d393a223
[sneha suman] <snehasuman1119@gmail.com>
2026-05-10 13:44:21 +0530

    PRint 1 to N

---
+++ Rev Prac/LoopOTN.java
@@ -0,0 +1,14 @@
+// package Rev Prac;
+import java.util.*;
+public class LoopOTN {
+    public static void main(String[] args){
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter a number: ");
+        // int n = sc.nextInt();
+        
+        int n = 10;
+        for(int i=1; i<=n; i++){
+            System.out.println(i);
+        }
+    }
+}
